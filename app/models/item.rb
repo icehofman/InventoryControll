e0192f7c97cf6447a428eef5f67aa1e9a2570638
Israel Hofman <ihofman@stefanini.com>
2014-06-02 16:02:52 -0300

    Validation stock.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,13 @@
 class Item < ActiveRecord::Base
 	belongs_to :order
   	belongs_to :product
+  	belongs_to :stock
   	validates_presence_of :product, :quantity
+
+  	validates_numericality_of  :quantity, :greater_than => 0, :less_than => :stock_quantity_value,:on => :create
+
+  	def stock_quantity_value
+  		s = Stock.find(product_id).quantity
+  		return s
+  	end
 end
