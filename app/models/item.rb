60420b5728eca5c85542e955d02d4b016a01dc27
Israel Hofman <ihofman@stefanini.com>
2014-06-05 22:16:15 -0300

    Before bootstrap.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ActiveRecord::Base
-	belongs_to :order
+	  belongs_to :order
   	belongs_to :product
+
   	validates_presence_of :product, :quantity
 
   	validates_numericality_of  :quantity, :greater_than => 0, :less_than_or_equal_to => :stock_quantity_value, on: :create
@@ -8,13 +9,10 @@ class Item < ActiveRecord::Base
   	validates_numericality_of  :quantity, :greater_than_or_equal_to => :stock_quantity_value, :less_than_or_equal_to => :item_quantity_value, on: :update
   	
   	def stock_quantity_value
-  		s = Stock.find(product_id).quantity
-  		return s
+  		Stock.find(product_id).quantity
   	end
 
-	def item_quantity_value
-  		s = Item.find(id).quantity
-  		return s
+    def item_quantity_value
+  		Item.find(id).quantity
   	end
-
 end
