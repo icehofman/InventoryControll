20dee83ee51e2810f075529c3ff9a0a31aa9fa6e
Israel Hofman <ihofman@stefanini.com>
2014-06-19 19:16:36 -0300

    Finish test.

--- test/controllers/stocks_controller_test.rb
+++ test/controllers/stocks_controller_test.rb
@@ -4,13 +4,7 @@ class StocksControllerTest < ActionController::TestCase
   setup do
     @stock = stocks(:one)
   end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:stocks)
-  end
-
+  
   test "should get new" do
     get :new
     assert_response :success
